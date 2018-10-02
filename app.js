94305ae48ca14305bcba8362697989493833a3db
Ryan Peacock <ryanxtry@gmail.com>
2018-10-02 20:18:08 +0000

    first commit

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app = express()
 
 
 // APP CONFIG
-// mongoose.connect("mongodb://localhost/restful_blog_app")
-mongoose.connect("mongodb://peac93:VioletSpice93@ds119853.mlab.com:19853/restfulblog")
+var url = process.env.DATABASEURL || "mongodb://lolhost/yelp_camp_v10"
+mongoose.connect(url)
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
